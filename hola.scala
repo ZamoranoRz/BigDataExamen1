87ab3ac2f4256c21ac3573a25a6296387f1e8a89
Jose Alfonso <poncho@MBP-de-Jose-2.tectijuana.edu.mx>
2018-09-10 17:57:47 -0700

    otra prueba

---
+++ hola.scala
@@ -0,0 +1,11 @@
+def isPrime(num:Int): Boolean = {
+    for(n <- Range(2, num)){
+        if(num%n == 0){
+            return false
+        }
+    }
+    return true
+}
+
+println(isPrime(10))
+println(isPrime(23))
